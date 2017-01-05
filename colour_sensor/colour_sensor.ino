51a2edaf64bbd5bd8a5d919d57841d7f09b3445e
Hannah Hazi <hannah.hazi@cantab.net>
2017-01-05 15:13:29 +0000

    Updated colours with eye perception skewing based on datasheet for TCS sensor and some plots I found online of human cone sensing peaks. Seems to work a bit better.

--- colour_sensor/colour_sensor.ino
+++ colour_sensor/colour_sensor.ino
@@ -4,7 +4,7 @@
 
 #define PIN 6
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(4, PIN, NEO_GRB + NEO_KHZ800);
 
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
 
@@ -43,6 +43,14 @@ void setup() {
 
 uint16_t red, green, blue, clearness; // make some global variables to store measured data
 
+void colorWipe(uint32_t c, uint8_t wait) {
+    for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+    }
+  }
+
 void loop() {
   tcs.setInterrupt(false); // turn on the sensor's white LED, for consistent readings
   delay(60); // takes some time to read the data
@@ -58,10 +66,13 @@ void loop() {
 
   // code to turn the measured values into 0 - 255 range
   // NOT in a separate function def because returning got messy...
-  uint32_t sum = red + green + blue + clearness;
   float r, g, b;
   r = red;      g = green;    b = blue;
-  r /= sum;         g /= sum;         b /= sum;
+  r = r*0.755;       g = g*1.049;     b = b*1.196; // code I worked out to adjust
+  // to make it more like the human eye's perception of colour (see notebook)
+
+  uint32_t sum = r + g + b + clearness;
+  r /= sum;         g /= sum;         b /= sum; 
   r = r*256;        g = g*256;        b = b*256;
   int r_show, g_show, b_show;
   r_show = (int)r;  g_show = (int)g;  b_show = (int)b;
@@ -74,7 +85,9 @@ void loop() {
   Serial.print("\tG:\t"); Serial.print(gammatable[g_show]);
   Serial.print("\tB:\t"); Serial.print(gammatable[b_show]);   Serial.print("\n");
 
-  strip.setPixelColor(0, gammatable[r_show], gammatable[g_show], gammatable[b_show]);
+  
+
+  colorWipe(strip.Color(gammatable[r_show], gammatable[g_show], gammatable[b_show]), 500);
   strip.show();
 
   delay(5000);
